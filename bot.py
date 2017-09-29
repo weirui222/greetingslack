f4699bc28bcaae7f9a7ec062e1b71587e11ef751
Luke Thompson <luke.thompson@concur.com>
2017-09-29 10:42:07 -0700

    change logger levels

--- bot.py
+++ bot.py
@@ -67,6 +67,10 @@ def on_open(ws):
 
 if __name__ == "__main__":
     logger = logging.getLogger("slack-bot")
+    logger.setLevel(DEBUG)
+
+    wsLogger = logging.getLogger("websocket")
+    wsLogger.setLevel(DEBUG)
 
     r = start_rtm()
     logger.info("WebSocket URL:", r)
